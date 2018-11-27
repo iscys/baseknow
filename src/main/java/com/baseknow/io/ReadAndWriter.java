1182991a4592118f0073191c2efe60e145c9c795
iscys <iscys@192.168.1.144>
2018-11-27 23:47:48 +0800

    pp,

---
+++ src/main/java/com/baseknow/io/ReadAndWriter.java
@@ -0,0 +1,60 @@
+package com.baseknow.io;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import com.baseknow.utils.ResourceUtils;
+
+/**
+ * IO操作对于文件的读和写操作；
+ * @author iscys
+ *
+ */
+public class ReadAndWriter {
+	
+	public static void main(String[] args) throws Exception {
+		
+		File file = ResourceUtils.getFile("readme.txt");
+		//获取文件的输入流；
+		FileInputStream fileInput =new FileInputStream(file);
+		int read = fileInput.read();
+		System.out.println(read);
+		int read2 = fileInput.read();//每读一次，只会读取一个字节，指针往后移动一次，当没有数据的时候会返回-1
+		System.out.println(read2);
+		fileInput.close();
+		
+		///-------------华丽的分界线,循环读取数据---------------///
+		File file2 = ResourceUtils.getFile("readme.txt");
+		//获取文件的输入流；
+		FileInputStream fileInput2 =new FileInputStream(file);
+		int h;
+		while((h=fileInput2.read())!=-1) {
+			System.out.println(h);
+		}
+		fileInput2.close();
+		
+		//--------------输入流与输出流进行文件的复制粘贴----------///
+		File file3 = ResourceUtils.getFile("readme.txt");
+		//获取文件的输入流；
+		FileInputStream fileInput3 =new FileInputStream(file);
+		//获取文件的输出流
+		FileOutputStream out =new FileOutputStream("me.txt");
+		int x;
+		while((x =fileInput3.read())!=-1) {
+			//不断的写入
+			out.write(x);
+		}
+		out.close();
+		fileInput3.close();
+		
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/baseknow/utils/ResourceUtils.java
@@ -0,0 +1,88 @@
+package com.baseknow.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+import org.springframework.util.StringUtils;
+
+
+public class ResourceUtils {
+	
+	
+	/**
+	 * 加载项目中的文件转换为文件File对象
+	 * 如果文件在classpath 目录下，resourceName = classes下的相对路径 eg->spring/applicationContext.xml
+	 * 如果查找的文件在磁盘中，resourceName=磁盘绝对路径：rg->D:\\tomcat7.0.59\\apache-tomcat-7.0.59\\
+	 * 首先先会到项目路径下找
+	 * 然后才会到系统文件中去找
+	 * ..可以返回上一级目录
+	 * @throws Exception 
+	 */
+	
+	public static File getFile(String resourceName) throws Exception {
+		
+		notNull(resourceName, "文件名不能为空");
+		ClassLoader loader = getClassLoader();
+		//判断是否在classpath目录下
+		URL url = loader.getResource(resourceName);
+		if(url==null) {
+			//去磁盘中获取文件
+			File file = new File(resourceName);
+			if(!file.exists()) {throw new IOException("文件不存在");}
+			return file;
+		}
+		else {
+			//创建classpath 目录下的文件对象,utils使用spring的
+			URI uri = new URI(StringUtils.replace(url.toString(), " ", "%20"));
+			return new File(uri.getSchemeSpecificPart());
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 获取类加载器
+	 */
+	
+	public static  ClassLoader getClassLoader() {
+		
+		ClassLoader loader = null;
+		try {
+			loader = Thread.currentThread().getContextClassLoader();
+		}catch(Exception e) {}
+		if(loader==null) {
+		 loader = ResourceUtils.class.getClassLoader();
+		}
+		return loader;
+	}
+	
+	/**
+	 * 检测抛异常
+	 */
+	
+	public static void notNull(Object object, String message) {
+		if (object == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+	
+
+}
