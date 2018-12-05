25bad54703ba9399d3035a9f39247fab2cbc7e70
cys <cys@wx.huimai365.com>
2018-12-05 10:34:52 +0800

    objectinputstream

--- src/main/java/com/baseknow/io/ObjectStream.java
+++ src/main/java/com/baseknow/io/ObjectStream.java
@@ -1,5 +1,16 @@
 package com.baseknow.io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import com.baseknow.testclass.TestClass;
+import com.baseknow.utils.ResourceUtils;
+
 /**
  * 对象输入输出流
  * @author iscys
@@ -7,4 +18,24 @@ package com.baseknow.io;
  */
 public class ObjectStream {
 
+	
+	public static void main(String[] args) throws Exception {
+		
+		File file = ResourceUtils.getFile("readme.txt");
+		OutputStream out =new FileOutputStream(file);
+		ObjectOutputStream obj =new ObjectOutputStream(out);
+		obj.writeObject(new TestClass("cys","sss"));
+		obj.close();
+		out.close();
+		InputStream ins =new FileInputStream(file);
+		ObjectInputStream inn =new ObjectInputStream(ins);
+		TestClass readObject = (TestClass) inn.readObject();
+		System.out.println(readObject);
+		ins.close();
+		inn.close();
+		
+	}
+	
+	
+	
 }
--- src/main/java/com/baseknow/testclass/TestClass.java
+++ src/main/java/com/baseknow/testclass/TestClass.java
@@ -1,8 +1,37 @@
 package com.baseknow.testclass;
 
-public class TestClass {
+import java.io.Serializable;
+
+public class TestClass implements Serializable{
+	@Override
+	public String toString() {
+		return "TestClass [name=" + name + ", age=" + age + "]";
+	}
+	private static final long serialVersionUID = -6427276488672757050L;
+
+	public TestClass(String name,String age){
+		this.name=name;
+		this.age=age;
+	}
+	private String name;
+	private String age;
 	
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
 	public String isAutoWrized() {
 		return "autoWrized..ok";
 	}
