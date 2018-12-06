26bae4861e0f3bf241c94e0827445bc91b3c3c8c
cys <cys@wx.huimai365.com>
2018-12-06 17:47:02 +0800

    OK

--- src/main/java/com/baseknow/nio/Niodoc.java
+++ src/main/java/com/baseknow/nio/Niodoc.java
@@ -1,10 +1,13 @@
 package com.baseknow.nio;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
@@ -17,6 +20,12 @@ import java.nio.file.StandardOpenOption;
 public class Niodoc {
 	
 	public static void main(String[] args)throws Exception {
+		
+	
+		charset();
+	}
+
+	public  static void method() throws IOException {
 		//利用通道完成文件的复制】1.7提供了 open 方法
 		FileChannel read = FileChannel.open(Paths.get("me.txt"), StandardOpenOption.READ);//读模式
 		FileChannel write = FileChannel.open(Paths.get("m21.txt"), StandardOpenOption.WRITE,StandardOpenOption.CREATE);//读模式
@@ -28,7 +37,29 @@ public class Niodoc {
 		}
 		read.close();write.close();
 		
-			
 	}
-
+	/**
+	 * 文件映射
+	 * @throws IOException 
+	 */
+	public  static void FileMap() throws IOException{
+		FileChannel read = FileChannel.open(Paths.get("E:\\1.iso"), StandardOpenOption.READ);//读模式
+		FileChannel write = FileChannel.open(Paths.get("E:\\2.iso"), StandardOpenOption.WRITE,StandardOpenOption.CREATE);//读模式
+		read.transferTo(0, read.size(), write);
+		
+	}
+	/**
+	 *字符编码
+	 * @throws IOException 
+	 */
+	public  static void charset() throws IOException{
+		FileChannel ch = FileChannel.open(Paths.get("me.txt"), StandardOpenOption.READ);//读模式
+		Charset forName = Charset.forName("GBK");
+		
+		ByteBuffer byteb =ByteBuffer.allocate(2048);
+		ch.read(byteb);
+		byteb.flip();
+		CharBuffer cha = forName.decode(byteb);
+		System.out.println(new String(cha.array()));
+	}
 }
