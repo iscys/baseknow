175ee0aa793a595d1b31b360e1eef813c264dbcc
iscys <iscys@192.168.1.144>
2018-12-07 00:01:14 +0800

    update

--- src/main/java/com/baseknow/string/StringDoc.java
+++ src/main/java/com/baseknow/string/StringDoc.java
@@ -4,8 +4,9 @@ public class StringDoc {
 
 	/**
 	 * 字符串的创建都会放在字符串常量池中；
-	 * 字符串字面量创建会将字符串放在常量池中 比如 String a ="qwe" ;String b="qwe"; b 的引用直接拿到的就是常量池中的
-	 * 引用；所以a=b;
+	 * 字符串字面量创建会将字符串放在常量池中 比如 String a ="qwe" ;String b="qwe";
+	 * a首先会在常量池中创建qwe， b会直接在常量池中拿到qwe 的引用，a,b 拿到的都是常量池中相同的引用
+	 * 所以a=b;
 	 * 而 new String("hello") 会先判断常量池中是否有此字符串，没有则创建一个，并且还会在堆内存中创建一个引用，并
 	 * 返回堆内存的引用，所以"hello" <> new String("hello")
 	 * @param args
