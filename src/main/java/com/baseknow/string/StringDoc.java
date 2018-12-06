d1279302a3d110be7c70465b028dd57c27b7ce94
iscys <iscys@192.168.1.144>
2018-12-06 23:47:32 +0800

    sw

--- src/main/java/com/baseknow/string/StringDoc.java
+++ src/main/java/com/baseknow/string/StringDoc.java
@@ -4,6 +4,13 @@ public class StringDoc {
 
 	
 	public static void main(String[] args) {
-		System.out.println(StringDoc.class);
+		String lo="lo";
+		String hello ="hello";
+		System.out.println(hello=="hel"+"lo");
+		System.out.println(hello=="hel"+lo);
+		System.out.println(hello== new String("hello"));
+		System.out.println(hello== new String("hello").intern());
+		System.out.println(new String("hello")== new String("hello"));
+		System.out.println(new String("hello").intern()== new String("hello").intern());
 	}
 }
