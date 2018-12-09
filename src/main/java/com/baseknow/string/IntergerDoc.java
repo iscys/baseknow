8071e85f6aa65ea4c3c80d39a716a63cc93d7625
iscys <iscys>
2018-12-09 11:21:10 +0800

    ok

--- src/main/java/com/baseknow/string/IntergerDoc.java
+++ src/main/java/com/baseknow/string/IntergerDoc.java
@@ -4,9 +4,13 @@ public class IntergerDoc {
 
     public static void main(String[] args) {
 
-        Integer integer = new Integer(3);
-        System.out.println(integer==4);
+        Integer a = 129;
+        Integer b = 129;
+        int c=129;
+        Integer d =new Integer(129);
+        System.out.println(a == b);
+        System.out.println(a ==c );
+        System.out.println(c ==d);
+
     }
-
-
 }
