e3a10ae7eb12d34e79b7dbcd9339f7664c04babb
iscys <iscys>
2018-12-09 08:56:37 +0800

    ok

--- src/main/java/com/baseknow/list/ArrayListDoc.java
+++ src/main/java/com/baseknow/list/ArrayListDoc.java
@@ -1,5 +1,16 @@
 package com.baseknow.list;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDoc {
 
+	
+	public static void main(String[] args) {
+		
+		List<String> li =new ArrayList<String>();
+		li.forEach((v)->{
+			System.out.println(v);
+		});
+	}
 }
