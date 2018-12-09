82af9f0639537e601a6ffd11ad617f7588329e41
iscys <iscys>
2018-12-09 14:53:12 +0800

    OK

--- src/main/java/com/baseknow/io/InputStreamReaderDoc.java
+++ src/main/java/com/baseknow/io/InputStreamReaderDoc.java
@@ -4,6 +4,7 @@ import com.baseknow.utils.ResourceUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 
 /**
@@ -15,13 +16,42 @@ public class InputStreamReaderDoc {
 
     public static void main(String[] args) throws Exception {
 
-        File file = ResourceUtils.getFile("readme.txt");
-        FileInputStream fileInputStream =new FileInputStream(file);
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"utf-8");
-        int h;
-        while (( h=inputStreamReader.read())!=-1){
-            System.out.println((char)h);
-        }
+       //x1();
+    	bytes();
 
+    }
+    
+
+
+     /**
+      * 解决乱码的问题
+      * @throws Exception
+      */
+    static void bytes() throws Exception{
+    	 File file = ResourceUtils.getFile("readme2.txt");
+    	 FileInputStream fileInputStream =new FileInputStream(file);
+    	 FileWriter fil =new FileWriter("me.txt");
+    	 InputStreamReader reader =new InputStreamReader(fileInputStream,"gbk");
+    	 int h;
+         while (( h=reader.read())!=-1){
+            fil.write(h);
+        	 System.out.println((char)h);
+             
+         }
+         fileInputStream.close();
+         reader.close();
+         fil.close();
+         
+    }
+    static void x1() throws Exception{
+    	 File file = ResourceUtils.getFile("readme.txt");
+         FileInputStream fileInputStream =new FileInputStream(file);
+         InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream,"utf-8");
+         int h;
+         while (( h=inputStreamReader.read())!=-1){
+             System.out.println((char)h);
+         }
+         fileInputStream.close();
+         inputStreamReader.close();
     }
 }
