1e5343cf1870ca332a3a0c802bb13e6e1472ac11
iscys <iscys>
2018-12-11 21:57:49 +0800

    tree map

--- src/main/java/com/baseknow/map/TreeMapDoc.java
+++ src/main/java/com/baseknow/map/TreeMapDoc.java
@@ -13,66 +13,93 @@ import java.util.TreeMap;
  *
  */
 public class TreeMapDoc<K,V> {
-//树的根节点
-private AclNode<K,V> root;
-//size
-private  int size;
-private Comparator<K> compa;
+	//树的根节点
+	private AclNode<K, V> root;
+	//size
+	private int size;
+	private Comparator<K> compa;
 
-public int size() {
-	return this.size;
-}
-public boolean isEmpty() {
-	return size==0?false:true;
-}
-public int compare(K a,K b) {
-	if(compa !=null) {
-		return compa.compare(a, b);
-	}else {
-		@SuppressWarnings("unchecked")
-		Comparable<K> c= (Comparable<K>) a;
-		return c.compareTo(b);
+	public int size() {
+		return this.size;
 	}
-}
 
-public void put(K key,V value) {
-	if(null==root) {
-		root =new AclNode(key, value);
-		++size;
-	}else {
-		AclNode<K,V> p =root;
-		while(null !=p) {
-			int ex=compare(key,p.k);
-		if(ex==0) {
-			p.setValue(value);
-			break;
-		}else if(ex <0) {
-			
-			if(p.left==null) {
-				p.left= new AclNode(key, value);
-				++size;
-				break;
-			}else {
-				p=p.left;
-			}
-		}
-		else {
-			
-			if(p.right==null) {
-				p.right= new AclNode(key, value);
-				++size;
-				break;
-			}else {
-				p=p.right;
-			}
-			
-		}
-			
-			
-			
+	public boolean isEmpty() {
+		return size == 0 ? false : true;
+	}
+
+	public int compare(K a, K b) {
+		if (compa != null) {
+			return compa.compare(a, b);
+		} else {
+			@SuppressWarnings("unchecked")
+			Comparable<K> c = (Comparable<K>) a;
+			return c.compareTo(b);
 		}
 	}
-}
+
+	/**
+	 * 插入操作
+	 *
+	 * @param key
+	 * @param value
+	 */
+	public void put(K key, V value) {
+		if (null == root) {
+			root = new AclNode(key, value);
+			++size;
+		} else {
+			AclNode<K, V> p = root;
+			while (null != p) {
+				int ex = compare(key, p.k);
+				if (ex == 0) {
+					p.setValue(value);
+					break;
+				} else if (ex < 0) {
+
+					if (p.left == null) {
+						p.left = new AclNode(key, value);
+						++size;
+						break;
+					} else {
+						p = p.left;
+					}
+				} else {
+
+					if (p.right == null) {
+						p.right = new AclNode(key, value);
+						++size;
+						break;
+					} else {
+						p = p.right;
+					}
+
+				}
+
+
+			}
+		}
+	}
+
+	/**
+	 * 查找操作
+	 * 比较+递归
+	 */
+
+	public AclNode<K, V> getEntity(K key) {
+		AclNode<K, V> p = root;
+
+			while (p != null) {
+				int ex = compare(key, p.k);
+				if (ex < 0) {
+					p = p.left;
+				} else if (ex > 0) {
+					p = p.right;
+				} else {
+					return p;
+				}
+			}
+			return null;
+		}
 
 
 }
