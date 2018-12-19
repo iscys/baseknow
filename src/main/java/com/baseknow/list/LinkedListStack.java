36f15cbf6adaf9c44b82f4ed3d14fda47401d0ec
cys <cys107417>
2018-12-19 10:59:16 +0800

    ok

--- src/main/java/com/baseknow/list/LinkedListStack.java
+++ src/main/java/com/baseknow/list/LinkedListStack.java
@@ -1,9 +1,6 @@
 package com.baseknow.list;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
@@ -62,6 +59,77 @@ public class LinkedListStack {
 
     }
 
+
+    /**
+     *请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+     *单链表
+     * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+     *
+     *     4 -> 5 -> 1 -> 9
+     * 示例 1:
+     *
+     * 输入: head = [4,5,1,9], node = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+     */
+
+    public static  void deleteNode(ListNode node) {
+        //单链表无法寻找到节点的上一个节点
+
+        ListNode nextNode =node.next;
+        node.val=nextNode.val;
+        node.next=nextNode.next;
+        nextNode.next=null;
+
+
+
+    }
+
+
+    public class ListNode {
+      int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+  }
+
+
+
+/**
+ *   给定一个链表，判断链表中是否有环。
+ *
+ *     为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ *
+ *
+ *
+ *     示例 1：
+ *
+ *     输入：head = [3,2,0,-4], pos = 1
+ *     输出：true
+ *     解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ */
+
+public boolean hasCycle(ListNode head) {
+
+    HashSet<ListNode> set =  new HashSet<>();
+    while(head !=null){
+        if(set.contains(head)){
+            return true;
+        }else{
+            set.add(head);
+            head =head.next;
+        }
+    }
+    return false;
+
+}
+
+
+
+
+
+
+
     /**
      * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
      *
@@ -83,6 +151,7 @@ public class LinkedListStack {
     static class MinStack {
 
         /** initialize your data structure here. */
+        //使用两个队列进行数据的存储
         public  LinkedList<Integer>  list;
         public  LinkedList<Integer>  minStack = new LinkedList<>();
         Integer min;
