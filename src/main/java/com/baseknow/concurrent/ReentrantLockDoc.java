58e78033cbeb99b32e462394279b5b3cfcdea7f6
cys <cys107417>
2018-12-20 15:16:59 +0800

    ok

--- src/main/java/com/baseknow/concurrent/ReentrantLockDoc.java
+++ src/main/java/com/baseknow/concurrent/ReentrantLockDoc.java
@@ -3,6 +3,8 @@ package com.baseknow.concurrent;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -20,8 +22,33 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantLockDoc {
 
+   static ReentrantLock lock =new ReentrantLock();
+
     public static void main(String[] args) throws Exception {
-        ReentrantLock lock =new ReentrantLock();
+
+
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               lock.lock();
+                System.out.println("1111");
+                lock.unlock();
+           }
+       }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println("2222");
+
+            }
+        }).start();
+
+
+
+
+
         Condition isFull = lock.newCondition();
         Condition isEmpty = lock.newCondition();
         //CyclicBarrier
@@ -29,8 +56,9 @@ public class ReentrantLockDoc {
         count.countDown();
         count.countDown();
         count.countDown();
-
+        count.countDown();
         count.await();
         System.out.println("ss");
+
     }
 }
