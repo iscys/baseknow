8be70ff2ef35c4d7108baa9ce7978a3b9b1676cf
iscys <iscys>
2018-12-22 12:06:12 +0800

    queue

--- src/main/java/com/baseknow/concurrent/ReentrantLockDoc.java
+++ src/main/java/com/baseknow/concurrent/ReentrantLockDoc.java
@@ -20,6 +20,13 @@ import java.util.concurrent.locks.ReentrantLock;
  *
  * condition 代替 原始的wait(). notify().notifyAll();
  * cnnlogs:https://www.cnblogs.com/iscys/p/10152885.html
+ *
+ * 在线程未unpack 且 tryAcquire() 不等于1 的时候 lock.lock 一直处于阻塞状态
+ * 这样就保证了线程的安全性；
+ *
+ *
+ * unlock会将state 致为0  且将队列的下一个线程unpack ，这样下一个线程就可以
+ * tryAcquire() 拿到执行权 1  ,lock 方法将不在堵塞；
  */
 public class ReentrantLockDoc {
 
