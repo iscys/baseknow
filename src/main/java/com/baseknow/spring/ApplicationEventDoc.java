dff08a7bfd7870d1aa48f648d02e86bdf8936e52
iscys <iscys>
2018-12-25 22:18:32 +0800

    applicationListener

---
+++ src/main/java/com/baseknow/spring/ApplicationEventDoc.java
@@ -0,0 +1,14 @@
+package com.baseknow.spring;
+
+import org.springframework.context.ApplicationEvent;
+
+public class ApplicationEventDoc extends ApplicationEvent {
+
+
+    public ApplicationEventDoc(Object source) {
+        super(source);
+    }
+
+
+
+}
--- src/main/java/com/baseknow/spring/ApplicationListenerDoc.java
+++ src/main/java/com/baseknow/spring/ApplicationListenerDoc.java
@@ -3,14 +3,23 @@ package com.baseknow.spring;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationListenerDoc implements ApplicationListener {
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println(event);
         //在spring容器加载完之后进行触发此操作
-        if(ContextRefreshedEvent.class.getName().equals(event.getClass())){
-
+        if(ContextRefreshedEvent.class.getName().equals(event.getClass().getName())){
+            System.err.println(event);
         }
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:springTest/applicationListener.xml");
+
+        context.publishEvent(new ApplicationEventDoc("aaa"));
     }
 }
