3d7ae3e6319c6cca63801d228e7b01df33bcf3ad
cys <cys107417>
2018-12-29 16:38:19 +0800

    ok

---
+++ src/main/java/com/baseknow/javassist/Javassist.java
@@ -0,0 +1,9 @@
+package com.baseknow.javassist;
+
+import javassist.ClassPool;
+
+public class Javassist {
+    public static void main(String[] args) {
+        ClassPool pool =ClassPool.getDefault();
+    }
+}
