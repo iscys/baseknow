3250edbf113d0e9932752bc30287057f70f36469
iscys <iscys>
2018-12-30 19:31:45 +0800

    queue

--- src/main/java/com/baseknow/netty/NettyDoc.java
+++ src/main/java/com/baseknow/netty/NettyDoc.java
@@ -8,8 +8,10 @@ public class NettyDoc {
     public static void main(String[] args)throws Exception {
         NettyClient client = new NettyClient("127.0.0.1", 9077);
         Channel channel = client.getChannel();
-        ChannelFuture future = channel.writeAndFlush("我是你爸爸的爸爸");
-        channel.closeFuture().sync();
+        ChannelFuture future = channel.writeAndFlush("我是你爸爸的爸爸 \r\n");
+        //channel.closeFuture().sync();
+       client.destory();
+
 
     }
 
