ff67b78c4e89c0406e845f09e39ed4c7803081c8
iscys <iscys>
2018-12-30 19:30:59 +0800

    queue

--- src/main/java/com/baseknow/dubbo/DubboMain.java
+++ src/main/java/com/baseknow/dubbo/DubboMain.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DubboMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:dubbo/dubbo.xml");
+        Thread.sleep(10000000);
 
     }
 }
