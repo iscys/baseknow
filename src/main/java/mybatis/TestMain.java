9c2e44a8f15e1fa0d0a4bdaef8b64394c533b33d
iscys <iscys>
2019-01-08 23:15:18 +0800

    queue

---
+++ src/main/java/mybatis/TestMain.java
@@ -0,0 +1,12 @@
+package mybatis;
+
+import com.baseknow.dubbo.IdubboService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestMain {
+
+    @Autowired
+    private IdubboService service;
+
+
+}
