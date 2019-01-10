63331dc7479e1f53daf3771a7f673fb0bf23f170
cys <cys107417>
2019-01-10 17:33:44 +0800

    ok

--- src/main/java/mybatis/MapperFactoryBean.java
+++ src/main/java/mybatis/MapperFactoryBean.java
@@ -2,13 +2,16 @@ package mybatis;
 
 import org.springframework.beans.factory.FactoryBean;
 
+/**
+ * 实现FactoryBean 接口，实现灵活的bean 装载
+ */
 public class MapperFactoryBean implements FactoryBean {
 
     private Class mapperInterface;
 
     @Override
     public Object getObject() throws Exception {
-
+        //代理生成实现类
         return MybatisProxy.getProxy(mapperInterface);
     }
 
