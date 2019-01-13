c92e7fa253a0bc621835b1feff7a7467e29fdd05
iscys <iscys>
2019-01-13 22:40:49 +0800

    queue

--- src/main/java/mybatis/MybatisProxy.java
+++ src/main/java/mybatis/MybatisProxy.java
@@ -3,6 +3,10 @@ package mybatis;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MybatisProxy implements InvocationHandler {
 
@@ -16,8 +20,43 @@ public class MybatisProxy implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-
-        return "1";
+        String methodName =method.getName();
+        System.out.println(args);
+        fun1();
+        return methodName;
     }
+
+
+    /**
+     * preparestatement
+     * @throws Exception
+     */
+    public static void fun1() throws Exception{
+        //加载驱动类
+        Class.forName("com.mysql.jdbc.Driver");
+        String url ="jdbc:mysql://localhost:3306/iscys";
+        String username="root";
+        String password ="123123";
+        //获得连接
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        conn.setAutoCommit(false);//开启事务
+        System.out.println(conn);
+        //获取statement
+        String sql ="select *from user where name= ?";
+        PreparedStatement pstm = conn.prepareStatement(sql);
+        pstm.setString(1,"cys");
+
+        pstm.executeQuery();
+        ResultSet resultSet = pstm.getResultSet();
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("area"));
+        }
+
+        conn.commit();
+        //   Show ProcessList ---->mysql query obtain connection thread
+        conn.close();
+    }
+
 }
+
--- src/main/java/mybatis/TestMain.java
+++ src/main/java/mybatis/TestMain.java
@@ -1,6 +1,8 @@
 package mybatis;
 
 import com.baseknow.dubbo.IdubboService;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,9 @@ public class TestMain {
 
 
     public void test(){
-
+       // MapperFactoryBean
+        //SqlSessionFactoryBean
+        System.out.println(service.sayBye("1","1"));
         System.out.println(service.getClass().getName());
     }
 
