ea6502925e9d8c5636fa645877ba4f1f958cfc7c
iscys <iscys>
2019-02-09 11:37:28 +0800

    queue

---
+++ src/main/java/com/baseknow/faceTest/JavaRunMachine.java
@@ -0,0 +1,48 @@
+package com.baseknow.faceTest;
+
+/**
+ * java  运行机制
+ * 1.连接过程
+ * 为静态变量赋予初始值，null 与0；这里的赋初始值并不是我们赋予的值；
+ * nn = null;
+ * a=0;
+ * b=0;
+ *
+ * 2.初始化过程
+ * nn =new JavaRunMachine() -> a=1 ;b=1;
+ * a=1; b= 9;
+ *
+ *
+ */
+public class JavaRunMachine {
+
+
+    public static JavaRunMachine nn =new JavaRunMachine();
+    public  static int a =4 ;
+
+    public static  int b=9;
+
+    static {
+        System.out.println("静态代码块");
+        a++;
+        b++;
+    }
+
+
+    JavaRunMachine(){
+        System.out.println("构造函数");
+        a++;
+        b++;
+    }
+    public static JavaRunMachine getInstance(){
+        return nn;
+    }
+
+
+    public static void main(String[] args) {
+        JavaRunMachine nn = getInstance();
+        System.out.println(nn.a);
+        System.out.println(nn.b);
+
+    }
+}
--- src/main/java/com/baseknow/faceTest/MinStack.java
+++ src/main/java/com/baseknow/faceTest/MinStack.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 
 /**
  * leetcode 最小栈
- *
  * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
  *
  * push(x) -- 将元素 x 推入栈中。
