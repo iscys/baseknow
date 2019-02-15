012afa77890357396bd3b7d0011a166146853c0c
iscys <iscys>
2019-02-16 00:10:11 +0800

    queue

--- src/main/java/com/baseknow/faceTest/Leetcode102.java
+++ src/main/java/com/baseknow/faceTest/Leetcode102.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ *
+ *
  * 二叉树的层次遍历 facebook
  * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
  *
@@ -25,6 +27,11 @@ import java.util.List;
  * ]
  */
 public class Leetcode102 {
+    /**
+     * 1。记录当前层的长度
+     * @param root
+     * @return
+     */
 
     public List<List<Integer>> levelOrder(TreeNode root) {
 
@@ -35,8 +42,10 @@ public class Leetcode102 {
 
         if(root !=null) {
             queue.offer(root);
+
+            //BFS 模板
         while(!queue.isEmpty()) {
-            int size = queue.size();
+            int size = queue.size();//当前层的长度
             List<Integer> a = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode poll = queue.poll();
