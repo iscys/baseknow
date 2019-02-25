622065afcfe1800c8785409c82686c12563d0860
cys <cys107417>
2019-02-25 17:39:21 +0800

    ok

--- src/main/java/com/baseknow/spi/dubbospi/DubboMain.java
+++ src/main/java/com/baseknow/spi/dubbospi/DubboMain.java
@@ -2,14 +2,22 @@ package com.baseknow.spi.dubbospi;
 
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
 
+import java.lang.reflect.Constructor;
+
 public class DubboMain {
+  public   DubboMain(String a){
+        System.out.println(a);
+    }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         ExtensionLoader<DubboSPI> extensionLoader =
                 ExtensionLoader.getExtensionLoader(DubboSPI.class);
         DubboSPI optimusPrime = extensionLoader.getExtension("DubboSPI");
         optimusPrime.sayHello();
+
+        Class<DubboMain> dubboMainClass = DubboMain.class;
+        Constructor<DubboMain> constructor = dubboMainClass.getConstructor(String.class);
+        constructor.newInstance("rr");
 
     }
 }
