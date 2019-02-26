2bd75df662f2d600ffa975b5143eb9164d8dd001
cys <cys107417>
2019-02-26 10:46:25 +0800

    ok

--- src/main/java/com/baseknow/faceTest/BinarySearch.java
+++ src/main/java/com/baseknow/faceTest/BinarySearch.java
@@ -46,20 +46,16 @@ public class BinarySearch {
 
     public static int binarySearch2(int[] array,int target){
         Arrays.sort(array);
-
         int left =0;
         int right =array.length-1;
         int mid =0;
         while(left<=right){
             mid =(left+right)>>1;
-            if(array[mid]==target) return mid;
-            else if(array[mid]>target)
-                right =mid-1;
-            else
-                left =mid+1;
+            if(array[mid] ==target) return mid;
+            else if(array[mid]>target) right =mid-1;
+            else left =mid+1;
         }
         return mid;
-
     }
 
 
