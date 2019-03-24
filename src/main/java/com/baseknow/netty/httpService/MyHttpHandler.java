8509eb63adebc8c0afbaf0199332f8c53cc0e3f7
iscys <iscys>
2019-03-24 13:02:24 +0800

    queue

--- src/main/java/com/baseknow/netty/httpService/MyHttpHandler.java
+++ src/main/java/com/baseknow/netty/httpService/MyHttpHandler.java
@@ -1,13 +1,9 @@
 package com.baseknow.netty.httpService;
-
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
-
 public class MyHttpHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
